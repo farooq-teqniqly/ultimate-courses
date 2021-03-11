09a0f4d6629aec53b9fda239a97c61dc56890619
farooq-teqniqly <49829654+farooq-teqniqly@users.noreply.github.com>
2021-03-11 13:07:28 -0600

    Rendering flows (#3)
    Co-authored-by: Farooq Mahmud <farooq.mahmud@marel.com>

--- ng-fundamentals/app/app.component.ts
+++ ng-fundamentals/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Passenger } from './passenger.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,15 +7,43 @@ import { Component } from '@angular/core';
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  title: string;
-  name: string;
+  passengers: Passenger[] = [
+    {
+      id: 1,
+      fullName: 'Farooq Mahmud',
+      checkedIn: true,
+      children: [ 
+        {
+          name: 'Yasin',
+          age: 11
+        },
+      {
+        name: 'Noor',
+        age: 14
+      }]
+    },
+    {
+      id: 2,
+      fullName: 'Emily Green',
+      checkedIn: false,
+      children: [
+        {
+          name: 'Andrew',
+          age: 13
+        }
+      ]
+    },
+    {
+      id: 3,
+      fullName: 'Sofia Mahmud',
+      checkedIn: true,
+      children: null
+    }
+  ];
+
+  title: string = 'Passengers';
 
   constructor() {
-    this.title = 'Ultimate Angular';
-    this.name = 'Farooq Mahmud';
-  }
-
-  handleClick(value: string) {
-    console.log(value);
+    
   }
 }
